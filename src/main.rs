2ad18e160d107362283961be5c74bf009e424199
YummyOreo <bobgim20@gmail.com>
2023-07-21 11:34:23 -0500

    feat: add a list of files and dirs

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,15 @@ use eyre::{eyre, Result};
 use ratatui::{
     backend::CrosstermBackend,
     prelude::Backend,
-    widgets::{Block, Borders},
+    text::Text,
+    widgets::{Block, Borders, List, ListItem},
     Frame, Terminal,
 };
-use std::{io, thread, time::Duration};
+use std::{
+    fs::{self, ReadDir},
+    io, thread,
+    time::Duration,
+};
 
 fn make_terminal() -> Result<Terminal<CrosstermBackend<io::Stdout>>> {
     enable_raw_mode()?;
@@ -38,10 +43,16 @@ impl App {
         Ok(matches!(e, event::Event::Key(k) if k.code == KeyCode::Char('q')))
     }
 
-    pub fn draw(&mut self, f: &mut Frame<'_, impl Backend>) {
+    pub fn draw(&mut self, f: &mut Frame<'_, impl Backend>, files: ReadDir) {
         let size = f.size();
-        let block = Block::default().title("Block").borders(Borders::ALL);
-        f.render_widget(block, size);
+        let mut items = vec![];
+        for file in files {
+            let text = Text::from(file.unwrap().file_name().into_string().unwrap());
+            let item = ListItem::new(text);
+            items.push(item);
+        }
+        let list = List::new(items);
+        f.render_widget(list, size);
     }
 }
 
@@ -51,7 +62,8 @@ async fn main() -> Result<()> {
     let mut app = App {};
 
     loop {
-        terminal.draw(|f| app.draw(f))?;
+        let files = fs::read_dir("./")?;
+        terminal.draw(|f| app.draw(f, files))?;
 
         let event_ready = tokio::task::spawn_blocking(|| event::poll(Duration::from_millis(250)));
         if event_ready.await?? && app.handle_input(event::read()?).await? {
