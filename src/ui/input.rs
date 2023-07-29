ded7d3a6f5c527d3f3e39a9d64e7049c43513993
YummyOreo <bobgim20@gmail.com>
2023-07-29 18:18:48 -0500

    feat: start migration of input

---
+++ src/ui/input.rs
@@ -0,0 +1,47 @@
+use std::path::PathBuf;
+
+use crossterm::event::{Event, KeyCode};
+
+use crate::Mode;
+
+pub fn match_keycode(mode: &Mode, current_file: PathBuf, input: KeyCode) -> InputResult {
+    match input {
+        KeyCode::Char(c) if mode != &Mode::Basic => InputResult::Mode(InputModeResult::AddChar(c)),
+        KeyCode::Enter if mode != &Mode::Basic => InputResult::Mode(InputModeResult::Execute),
+        KeyCode::Esc if mode != &Mode::Basic => {
+            InputResult::Mode(InputModeResult::ModeChange(Mode::Basic))
+        }
+        KeyCode::Up | KeyCode::Char('k') => InputResult::MoveUp,
+        KeyCode::Down | KeyCode::Char('j') => InputResult::MoveDown,
+        KeyCode::Char('q') => InputResult::Quit,
+        KeyCode::Char('c') => {
+            InputResult::Mode(InputModeResult::ModeChange(Mode::CreateFile(String::new())))
+        }
+        KeyCode::Char('r') => InputResult::Mode(InputModeResult::ModeChange(Mode::RenameFile(
+            current_file.clone(),
+            current_file.to_string_lossy().to_string(),
+        ))),
+        KeyCode::Char('d') => InputResult::Mode(InputModeResult::ModeChange(Mode::DeleteFile(
+            current_file,
+            String::new(),
+        ))),
+        _ => InputResult::Skip,
+    }
+}
+
+pub enum InputResult {
+    MoveUp,
+    MoveDown,
+
+    Mode(InputModeResult),
+
+    Quit,
+    Skip,
+}
+
+pub enum InputModeResult {
+    ModeChange(Mode),
+    AddChar(char),
+    RemoveChar,
+    Execute,
+}
--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,12 +1,21 @@
-use std::io;
+use std::{fs::DirEntry, io, path::PathBuf};
 
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyEventKind},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use eyre::Result;
-use ratatui::{prelude::CrosstermBackend, Terminal};
+use eyre::{eyre, Result};
+use ratatui::{
+    prelude::{Backend, Constraint, CrosstermBackend, Direction, Layout},
+    style::{Color, Style},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+
+use crate::Mode;
+
+pub mod input;
 
 pub fn make_terminal() -> Result<Terminal<CrosstermBackend<io::Stdout>>> {
     enable_raw_mode()?;
@@ -25,4 +34,31 @@ pub fn restore_terminal(mut terminal: Terminal<CrosstermBackend<io::Stdout>>) ->
     )?;
     terminal.show_cursor()?;
     Ok(())
+}
+
+pub struct UiState {
+    files: Vec<DirEntry>,
+    path: PathBuf,
+    selected: usize,
+    max: usize,
+    mode: Mode,
+}
+
+impl UiState {
+    pub async fn input(&self, input: Event) -> input::InputResult {
+        if let Event::Key(key_event) = input {
+            if key_event.kind == KeyEventKind::Release {
+                return input::InputResult::Skip;
+            }
+            return input::match_keycode(
+                &self.mode,
+                self.files
+                    .get(self.selected)
+                    .expect("should be there")
+                    .path(),
+                key_event.code,
+            );
+        }
+        input::InputResult::Skip
+    }
 }
