bbbda0d0d9314c6d98d5b4683fd95e9290b43925
YummyOreo <bobgim20@gmail.com>
2023-08-17 18:43:23 -0500

    feat: add better error message

--- src/filesystem/read.rs
+++ src/filesystem/read.rs
@@ -3,7 +3,7 @@ use std::{
     path::PathBuf,
 };
 
-use eyre::{Report, Result};
+use eyre::{Context, Report, Result};
 
 pub enum ReadRes {
     FallBack { error: Report, files: Vec<DirEntry> },
@@ -11,13 +11,20 @@ pub enum ReadRes {
 }
 
 pub fn read_with_fallback(path: &PathBuf, fallback: PathBuf) -> Result<ReadRes> {
-    match fs::read_dir(path) {
+    match fs::read_dir(path).wrap_err_with(|| {
+        format!(
+            "Could not read path: \"{}\". Defaulting to \"./\"",
+            path.to_str().unwrap()
+        )
+    }) {
         Ok(t) => Ok(ReadRes::Read(
             t.collect::<Result<Vec<DirEntry>, std::io::Error>>()?,
         )),
         Err(e) => Ok(ReadRes::FallBack {
-            error: e.into(),
-            files: fs::read_dir(&fallback)?.collect::<Result<Vec<DirEntry>, std::io::Error>>()?,
+            error: e,
+            files: fs::read_dir(fallback)
+                .wrap_err_with(|| format!("Could not read path: \"{}\"", path.to_str().unwrap()))?
+                .collect::<Result<Vec<DirEntry>, std::io::Error>>()?,
         }),
     }
 }
